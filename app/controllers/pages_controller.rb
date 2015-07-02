702e798d1d365bfe95ff582fd8a3eab52d0ca8db
Marion Grandjean <grandjean.marion@gmail.com>
2015-07-02 12:41:17 +0200

    kkjk

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @cities = Institute.select(:city).distinct.map { |institute| institute.city }
+    @categories = Service.select(:category).distinct.map { |service| service.category }
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :institutes do
-    resources :services, only: [:new, :create, :edit, :update, :destroy]
+    resources :services, only: [:show, :new, :create, :edit, :update, :destroy]
   end
 
   devise_for :users
