eec37b53f678523f60abd2f303aa0fbb14a77541
Alex Sellam <alex.sellam@gmail.com>
2015-07-02 17:58:49 +0200

    thomas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    # @cities = Institute.select(:city).distinct.map { |institute| institute.city }
-    # @categories = Service.select(:category).distinct.map { |service| service.category }
+    @cities = Institute.select(:city).distinct.map { |institute| institute.city }
+    @categories = Service.select(:category).distinct.map { |service| service.category }
   end
 end
