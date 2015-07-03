4bd22e3e1e2d500cc9c29aa4886da4f6c998bea6
Marion Grandjean <grandjean.marion@gmail.com>
2015-07-03 15:18:35 +0200

    created calendar & booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,29 @@
 class BookingsController < ApplicationController
 
   def index
-
   end
 
   def new
-    @booking = Booking.new
-    @institute = Institute.find(params[:institute_id])
-    @service = Service.find(params[:service_id])
   end
 
   def create
-
+    @institute = Institute.find(params[:institute_id])
+    @service = Service.find(params[:service_id])
+    @booking = current_user.bookings.new(params[:id])
+    @date = params[:booking][:date]
+    @time = params[:booking][:time]
+    year = @date.split("-")[0].to_i
+    month = @date.split("-")[1].to_i
+    day = @date.split("-")[2].to_i
+    hour = @time.split(":")[0].to_i
+    min = @time.split(":")[1].to_i
+    @booking.start_datetime = DateTime.new(year, month, day, hour, min, 0)
+    @booking.end_datetime = @booking.start_datetime + @service.duration
+    if @booking.save
+      redirect_to institute_path(@institute)
+    else
+      render :new
+    end
   end
 
 end
