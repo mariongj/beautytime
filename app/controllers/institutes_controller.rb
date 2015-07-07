44a0ee1e3ee458c658ff32d878c5b2fc5f7752ee
Alex Sellam <alex.sellam@gmail.com>
2015-07-08 01:31:53 +0200

    namespaced routes + business institutes controller done, bookings controller to do

--- app/controllers/institutes_controller.rb
+++ app/controllers/institutes_controller.rb
@@ -1,5 +1,5 @@
 class InstitutesController < ApplicationController
-  before_action :find_institute, only: [ :show, :edit, :update, :destroy ]
+  before_action :find_institute, only: [:show]
 
 
 
@@ -30,12 +30,9 @@ class InstitutesController < ApplicationController
       end
 
       @markers = Gmaps4rails.build_markers(@institutes) do |institute, marker|
-        if institute.latitude && institute.longitude
           marker.lat institute.latitude
           marker.lng institute.longitude
-        else
-          raise
-        end
+      end
 
       end
   end
@@ -50,46 +47,8 @@ class InstitutesController < ApplicationController
     # @bookings = @institute.services.bookings
   end
 
-  def new
-    @institute = Institute.new
-  end
 
-  def create
-    @institute = current_user.institutes.build(institute_params)
-    @institute.validated = 0
-    if @institute.save
-        redirect_to new_institute_service_path(@institute)
-      else
-      render :new
-    end
-  end
 
-  def edit
-    @institute
-  end
-
-  def update
-    @institute.update(institute_params)
-    redirect_to business_institute_path(@institute)
-  end
-
-  def destroy
-    @institute.destroy
-    redirect_to :back
-  end
-
-  def businesses
-    @institutes = current_user.institutes
-  end
-
-  def business
-    @institute = Institute.find(params[:id])
-    @services = @institute.services
-    @markers = Gmaps4rails.build_markers(@institute) do |institute, marker|
-      marker.lat institute.latitude
-      marker.lng institute.longitude
-    end
-  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,30 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
-  resources :institutes do
+  resources :institutes, only: [:index, :show] do
     resources :services, only: [:show, :new, :create, :edit, :update, :destroy]
-    collection do                       # collection => no restaurant id in URL
-    get 'businesses', to: "institutes#businesses"  # InstitutesController#businesses
-      end
-    member do                             # member => institute id in URL
-        get 'business', to: "institutes#business"  # InstitutesController#business
-      end
+    # collection do                       # collection => no restaurant id in URL
+    #   get 'businesses', to: "institutes#businesses"  # InstitutesController#businesses
+    # end
+    # member do                             # member => institute id in URL
+    #   get 'business', to: "institutes#business"  # InstitutesController#business
+    # end
   end
 
   resources :services, only: [] do
-    resources :bookings, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :bookings, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     resources :timetables, only: [:new, :create, :edit, :update]
   end
+
+  namespace :business do
+    resources :services, only: [] do
+      resources :bookings, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    end
+    resources :institutes do
+      resources :services, only: [:index, :show]
+    end
+  end
+
 
   resources :bookings, only: [:update, :edit, :destroy] do
     resources :reviews, only: [:new, :create]
@@ -24,9 +34,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:show, :edit, :update] do
-      member do
+    member do
       get 'bookings', to: "users#bookings"
       get 'institutes', to: "users#institutes"
-      end
+    end
   end
 end
