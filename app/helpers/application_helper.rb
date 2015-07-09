3beda8db8e84b9c3f9657b16f6fc4eb226c2f128
Dimitri Boukobza <dimitri.boukobza@gmail.com>
2015-07-09 11:57:34 +0200

    style modifier profil done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
 module ApplicationHelper
+
   def average_stars(institute)
-
-    stocked_reviews =[]
-    institute.reviews.each do |review|
-      stocked_reviews << review.rate
+    if institute.reviews.size == 0
+      return 0
+    else
+      stocked_reviews =[]
+      institute.reviews.each do |review|
+        stocked_reviews << review.rate
+      end
+      return stocked_reviews.inject(:+) / institute.reviews.size
     end
-
-    average = stocked_reviews.inject(:+) / institute.reviews.size
-    return average
   end
+
+
 end
+
