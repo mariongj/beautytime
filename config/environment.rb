90cb26b831764b6217c0c27511faa0b519e7f382
Alex Sellam <alex.sellam@gmail.com>
2015-07-10 11:56:51 +0200

    metatag fixed

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-DEFAULT_META = YAML.load_file(Rails.root.join('/config/meta.yml'))
+DEFAULT_META = YAML.load_file(Rails.root.join('config/meta.yml'))
